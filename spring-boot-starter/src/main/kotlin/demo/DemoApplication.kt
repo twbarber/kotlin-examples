4efe7aef3d9569c5318f6fb8c35ed81b43ffdfe0
Tyler Barber <tyler@rocketmiles.com>
2017-08-17 07:11:41 -0500

    Spring Boot App Working

--- spring-boot-starter/src/main/kotlin/demo/DemoApplication.kt
+++ spring-boot-starter/src/main/kotlin/demo/DemoApplication.kt
@@ -1,8 +1,22 @@
 package demo
 
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
 @SpringBootApplication
-class KotlinDemoApplication
+class DemoApplication
 
 fun main(args: Array<String>) {
-	SpringApplication.run(KotlinDemoApplication::class.java, *args)
+	SpringApplication.run(DemoApplication::class.java, *args)
+}
+
+@RestController
+class HelloController {
+
+	@GetMapping("/hello")
+	fun helloKotlin(): String {
+		return "hello world"
+	}
 }
